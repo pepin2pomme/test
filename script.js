270eb755ea0254886d6f5c5808ca01334f323541
milo <milosorelcpt@gmail.com>
2025-09-21 00:55:56 +0200

    modif 3eme button

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 const buttons = [
     { id: 1, label: "La réponse est psychose !" },
     { id: 2, label: "C quoi le genre de manga pour jeunes filles ?" },
-    { id: 3, label: "Bouton 3" },
-    { id: 4, label: "Bouton 4" },
-    { id: 5, label: "Bouton 5" },
+    { id: 3, label: "c'est qui milo ?"},
 ];
 
 const textes = [
     "après réfléxion, je dirais psychose",
-    "c'est les hentais !!"
+    "c'est les hentais !!",
+    "squelette très menaçant"
 ];
 
 // Fonction pour afficher les boutons
@@ -20,11 +19,13 @@ function displayButtons() {
         const btn = document.createElement('button');
         btn.textContent = button.label;
         btn.addEventListener('click', () => {
-            // On utilise l'index pour choisir l'image
             document.getElementById("imageachanger").src = `img/${i}.png`;
             document.getElementById("texteachanger").innerHTML = textes[i];
-            // ou si tes images sont nommées différemment :
-            // document.getElementById("imageachanger").src = `img/image_${i}.png`;
+            if (button.id === 3) {
+                document.body.classList.toggle('darkened');
+            } else {
+                document.body.classList.remove('darkened');
+            }
         });
         container.appendChild(btn);
     });
